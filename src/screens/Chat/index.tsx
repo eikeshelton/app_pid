32c7c60384e88a18c325b4e6879fbdc333a86184
eikeshelton <eikeshelton3@gmail.com>
2024-05-29 11:57:49 -0300

    alteraçoes eike 29/05/24

--- src/screens/Chat/index.tsx
+++ src/screens/Chat/index.tsx
@@ -1,11 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import {Container, Header, PictureProfile} from './style';
+import {
+  Container,
+  Header,
+  Name,
+  PictureContainer,
+  PictureProfile,
+  ProfilePicture,
+} from './style';
 import fotopropid from '../../assets/imagens/fotopropid.jpg';
 import BackButton from '../../components/BackButton';
 import {Input} from '../../components/Input/style';
 import CustomButton from '../../components/CustomizeButton';
 import {useAuth} from '../../hooks/auth';
 import {useRoute} from '@react-navigation/native';
+import {FlatList} from 'react-native';
+import fotoPerfil from '../../assets/imagens/fotoperfil.png';
 interface Params {
   selectedItem: {
     tipo_usuario: string;
@@ -15,9 +24,13 @@ interface Params {
     seguidos: number;
     bio: string;
     id: number;
-
-    // Adicione outras propriedades conforme necessário
   };
+}
+interface Mensagem {
+  id: number;
+  remetente_id: number;
+  destinatario_id: number;
+  texto: string;
 }
 export function Chat() {
   const [mensagem, setMensagem] = useState('');
@@ -26,19 +39,25 @@ export function Chat() {
   const route = useRoute();
   const params = route.params as Params; // Converter para o tipo esperado
   const {selectedItem} = params;
+  const [mensagens, setMensagens] = useState<Mensagem[]>([]);
   useEffect(() => {
-    // Criando uma nova conexão WebSocket
-    const ws = new WebSocket('ws://10.62.133.119:8000/ws');
-    // Definindo callbacks de eventos da conexão WebSocket
+    const ws = new WebSocket('ws://192.168.15.170:8000/ws');
+
     ws.onopen = () => {
       console.log('Conexão estabelecida com sucesso.');
-      // Enviando uma mensagem para o servidor ao abrir a conexão
       setWebsocket(ws);
     };
 
     ws.onmessage = event => {
-      console.log('Mensagem recebidaaaaa:', event.data);
-      // Tratando a mensagem recebida do servidor
+      console.log('Mensagem recebida:', event.data);
+      try {
+        // Analisa a mensagem JSON recebida
+        const data: Mensagem[] = JSON.parse(event.data);
+        console.log('Dados recebidos:', data);
+        setMensagens(data);
+      } catch (error) {
+        console.error('Erro ao analisar JSON:', error);
+      }
     };
 
     ws.onerror = error => {
@@ -49,28 +68,44 @@ export function Chat() {
       console.log('Conexão WebSocket fechada.');
     };
 
-    // Retornando uma função de limpeza para fechar a conexão WebSocket quando o componente for desmontado
     return () => {
       ws.close();
     };
   }, []);
+
   const sendMessage = () => {
-    if (websocket && mensagem.trim() !== '') {
-      const messageData = {
-        remetente_id: user.id, // Substitua pelo ID do remetente
-        destinatario_id: selectedItem.id, // Substitua pelo ID do destinatário
-        texto: mensagem.trim(),
+    if (mensagem.trim() !== '') {
+      const msg = {
+        remetente_id: user.id,
+        destinatario_id: selectedItem.id,
+        texto: mensagem,
       };
-      websocket.send(JSON.stringify(messageData));
+      websocket?.send(JSON.stringify(msg));
       setMensagem('');
     }
   };
+  const renderItem = ({item}: any) => (
+    <PictureContainer>
+      {item.foto_perfil ? (
+        <ProfilePicture source={{uri: item.foto_perfil}} resizeMode="contain" />
+      ) : (
+        <ProfilePicture source={fotoPerfil} resizeMode="contain" />
+      )}
+      <Name>{item.nome_remetente}</Name>
+      <Name>{item.texto}</Name>
+    </PictureContainer>
+  );
   return (
     <Container>
       <Header>
         <BackButton />
         <PictureProfile source={fotopropid} />
       </Header>
+      <FlatList
+        data={mensagens}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
       <Input
         onChangeText={text => setMensagem(text)}
         value={mensagem}
--- src/screens/Chat/style.ts
+++ src/screens/Chat/style.ts
@@ -1,4 +1,5 @@
 import {Dimensions} from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -20,4 +21,26 @@ export const PictureProfile = styled.Image`
   border-radius: 100px;
   height: ${PictureProfileHeight}px;
   width: ${PictureProfileWidth}px;
+`;
+export const PictureContainer = styled.View`
+  align-items: center;
+  justify-content: center;
+  border-radius: 24px;
+  margin-bottom: ${RFValue(10)}px;
+  margin-left: ${RFValue(10)}px;
+  height: ${RFValue(150)}px;
+  width: ${RFValue(130)}px;
+`;
+
+const ProfilePictureHeight = Dimensions.get('window').height * 0.1;
+const ProfilePictureWidth = Dimensions.get('window').width * 0.2;
+export const ProfilePicture = styled.Image`
+  border-radius: 100px;
+  height: ${ProfilePictureHeight}px;
+  width: ${ProfilePictureWidth}px;
+`;
+export const Name = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${({theme}) => theme.colors.colorWhite};
+  text-align: center;
 `;
