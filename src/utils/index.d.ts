e7de1d56b9ad2ae8226e990ec173ecf7c0105a5b
eikeshelton <eikeshelton3@gmail.com>
2024-09-15 14:45:51 -0300

    alteracoes eike 15/09/2024 14:45

--- src/utils/index.d.ts
+++ src/utils/index.d.ts
@@ -4,6 +4,5 @@ declare module '*.png';
 declare module '@env' {
   export const AWS_ACCESS_KEY_ID: string;
   export const AWS_SECRET_ACCESS_KEY: string;
-  export const url_key: string;
   // Declare outras variáveis de ambiente que você está usando
 }
